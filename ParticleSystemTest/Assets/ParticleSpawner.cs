e721a8edf804f4902a08c63fc15c6b900793d3b8
Emil Motrøen <e.motroen@gmail.com>
2023-02-16 14:12:20 +0100

    Spawnbox size auto sets based on the alpide size

--- ParticleSystemTest/Assets/ParticleSpawner.cs
+++ ParticleSystemTest/Assets/ParticleSpawner.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ParticleSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _particle;
     [SerializeField] private GameObject _spawnBox;
+	[SerializeField] private GameObject _alpide;
 
     public static int _numberOfParticles = 0;
     private int _maxParticles = 5;
+	private float _sizeOfBoxX;
+	private float _sizeOfBoxZ;
 
-    private void Start()
-    {
+	private void Start()
+	{
+		_sizeOfBoxX = _alpide.GetComponent<MeshRenderer>().bounds.size.x;
+		_sizeOfBoxZ = _alpide.GetComponent<MeshRenderer>().bounds.size.z;
+
+		_spawnBox.transform.localScale = new Vector3(_sizeOfBoxX, .2f, _sizeOfBoxZ);
+		Debug.Log($"x: {_sizeOfBoxX} z: {_sizeOfBoxZ}");
 	}
 
-    private void Update()
+	/// <summary>
+	/// TODO: Destroy instatiated particles after the hit the floor
+	/// </summary>
+	private void Update()
     {
 		if (_numberOfParticles < _maxParticles)
 		{
